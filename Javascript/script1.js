4fd398a664190d6ce517b8df0a3e3d573a011259
R2D2isHuman <petre.cosminpetrut97@gmail.com>
2018-11-24 15:10:41 +0200

    .js

---
+++ Javascript/script1.js
@@ -0,0 +1,18 @@
+// let tdA1 = document.querySelector("#A1");
+// let tdA2 = document.querySelector("#A2");
+// let tdA3 = document.querySelector("#A3");
+// let tdA4 = document.querySelector("#A4");
+// let tdA5 = document.querySelector("#A5");
+// let tdA6 = document.querySelector("#A6");
+// let tdA7 = document.querySelector("#A7");
+// let tdA8 = document.querySelector("#A8");
+// let tdA9 = document.querySelector("#A9");
+// let tdA10 = document.querySelector("#A10");
+
+let table={
+
+}
+
+for(let i = 0;i<=10;i++)
+    table['A'+'i'] = document.querySelector('#A'+i);
+
